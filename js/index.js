213b0b7064215063176c9b4c27426453d01f8ea5
mjds108 <37586895+mjds108@users.noreply.github.com>
2019-12-18 15:15:32 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+$(function(){
+ var swiper = new Swiper('.swiper-container', {
+   pagination: {               
+     el: '.swiper-pagination',
+     dynamicBullets: true,
+   },
+      autoplay: {
+    delay: 2500,
+     disableOnInteraction: false,
+   },
+ });
+})
+
